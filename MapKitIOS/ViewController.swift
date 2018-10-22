81fd9479015bd2d22ce4fa90844e1007f56f13c1
Fathur Rizki Rahmadi <fathurriskirahmadi@icloud.com>
2018-10-22 14:12:45 +0700

    Initial Commit

---
+++ MapKitIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapKitIOS
+//
+//  Created by Fathur Rizki Rahmadi on 22/10/18.
+//  Copyright © 2018 Fathur Rizki Rahmadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
